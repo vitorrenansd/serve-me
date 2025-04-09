8ab55cc8745b08111bace8890e7a740eeaf7979e
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-09 09:07:19 -0300

    Initial commit

---
+++ src/main/java/com/vitorrenansd/serveme/ServeMeApplication.java
@@ -0,0 +1,13 @@
+package com.vitorrenansd.serveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorrenansd/serveme/ServeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorrenansd.serveme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
