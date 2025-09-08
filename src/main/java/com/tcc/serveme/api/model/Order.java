d2eb3fc3287066c95d2f82623df41593464e0c91
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-09-08 13:22:48 -0300

    update: getters and setters to model classes

--- src/main/java/com/tcc/serveme/api/model/Order.java
+++ src/main/java/com/tcc/serveme/api/model/Order.java
@@ -9,4 +9,44 @@ public class Order {
     private List<OrderItem> items;
     private String notes;
     private LocalDateTime createdAt;
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public String getWaiter() {
+        return waiter;
+    }
+
+    public void setWaiter(String waiter) {
+        this.waiter = waiter;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/tcc/serveme/api/model/OrderItem.java
+++ src/main/java/com/tcc/serveme/api/model/OrderItem.java
@@ -5,5 +5,27 @@ public class OrderItem {
     private int quantity;
     private double unitPrice;
 
-    
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
