49f6309cf0de93d258fa9c0dfee6d26d2d22fc20
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-09-09 12:52:14 -0300

    update: added imports and autowired

--- src/main/java/com/tcc/serveme/api/controller/OrderController.java
+++ src/main/java/com/tcc/serveme/api/controller/OrderController.java
@@ -1,12 +1,18 @@
 package com.tcc.serveme.api.controller;
 
+import com.tcc.serveme.api.dto.OrderRequest;
+import com.tcc.serveme.api.service.OrderService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 @CrossOrigin("*")
 public class OrderController {
-    
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     // @PostMapping("/send")
     // send order to backend
