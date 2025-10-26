e9f9cdbb457227f1bd540bb76a98e9ab6034e8bb
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-26 16:52:08 -0300

    criado a tabela product e alguns records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product(id, name, price, inactive) VALUES
+(1, 'Batata frita', 20.49, FALSE),
+(2, 'Coca-cola Zero 600ml', 5.99, FALSE),
+(3, 'Coca-cola 600ml', 5.99, FALSE),
+(4, 'Pastel 4 queijos', 28.99, TRUE);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product (
+    id INT PRIMARY KEY,
+    name VARCHAR(40),
+    price NUMERIC(12,2),
+    inactive BOOLEAN
+);
