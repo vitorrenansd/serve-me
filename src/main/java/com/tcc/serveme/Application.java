dbc7e397a180cb70e818a688d57478e008cbd048
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-26 16:56:01 -0300

    testando o SELECT na tabela product

--- src/main/java/com/tcc/serveme/Application.java
+++ src/main/java/com/tcc/serveme/Application.java
@@ -2,10 +2,18 @@ package com.tcc.serveme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.tcc.serveme.api.repository.ProductRepository;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        ProductRepository pr = context.getBean(ProductRepository.class);
+
+        System.out.println(pr.findById(2L));
+        System.out.println(pr.findAll());
 	}
 }
