7d31c97a82fdbf56786769a621b6e9b3d2cffb86
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-29 23:47:29 -0300

    tabela category adicionada com campos basicos

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,5 @@
-CREATE TABLE product (
-    id INT PRIMARY KEY,
-    name VARCHAR(40),
-    price NUMERIC(12,2),
-    category VARCHAR(50),
+CREATE TABLE category (
+    id IDENTITY PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
     inactive BOOLEAN
 ); // EDITAR DEPOIS: Passar a utilizar category como uma chave de outra tabela
