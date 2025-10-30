d8b5243ece7eae532b4dfccebce1b4372a839621
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-29 22:34:58 -0300

    hot-fix: Package da classe main excluido erroneamente

--- src/main/java/com/tcc/serveme/Application.java
+++ src/main/java/com/tcc/serveme/Application.java
@@ -1,3 +1,5 @@
+package com.tcc.serveme;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
