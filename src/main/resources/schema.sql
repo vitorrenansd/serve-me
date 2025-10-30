7748b453e5433defe94c79202c4beeac26eb9984
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-29 23:48:07 -0300

    tabela product agora tem um FK de category

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,4 +2,16 @@ CREATE TABLE category (
     id IDENTITY PRIMARY KEY,
     name VARCHAR(40) NOT NULL,
     inactive BOOLEAN
-); // EDITAR DEPOIS: Passar a utilizar category como uma chave de outra tabela
+);
+
+CREATE TABLE product (
+    id IDENTITY PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    price NUMERIC(12,2) NOT NULL,
+    fk_category INT,
+    inactive BOOLEAN,
+
+    CONSTRAINT fk_product_category
+        FOREIGN KEY (fk_category)
+        REFERENCES category(id)
+);
