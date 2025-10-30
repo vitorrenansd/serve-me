c3230b32c393fc30c127fb42f53296cd63c85e29
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-29 23:48:57 -0300

    adicionados alguns dados para a tabela category

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,9 @@
-INSERT INTO product(id, name, price, category, inactive) VALUES
-(1, 'Batata frita', 20.49, 'Porções', FALSE),
-(2, 'Coca-cola Zero 600ml', 5.99, 'Bebidas', FALSE),
-(3, 'Coca-cola 600ml', 5.99, 'Bebidas', FALSE),
-(4, 'Pastel 4 queijos', 28.99, 'Porções', TRUE);
+INSERT INTO category(id, name, inactive) VALUES
+(1, 'Porções', FALSE),
+(2, 'Bebidas', FALSE);
+
+INSERT INTO product(id, name, price, fk_category, inactive) VALUES
+(1, 'Batata frita', 20.49, 1, FALSE),
+(2, 'Coca-cola Zero 600ml', 5.99, 2, FALSE),
+(3, 'Coca-cola 600ml', 5.99, 2, FALSE),
+(4, 'Pastel 4 queijos', 28.99, 1, TRUE);
